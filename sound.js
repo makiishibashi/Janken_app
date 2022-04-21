899a2368085bec052cd180b1a0aea72f4eaa2ab0
makiishibashi <greetings.from.maki625@gmail.com>
2022-04-21 20:47:27 +0900

    Janken version 03

--- sound.js
+++ sound.js
@@ -1,12 +1,38 @@
-(function (window, $) {
-  'use strict';
-  $.fn.useSound = function (_event, _id) {
-    var se = $(_id);
-    this.on(_event, function(){
-      se[0].currentTime = 0;
-      se[0].play();
-    });
-    return this;
-  };
-})(this, this.jQuery);
-$('.btn a').useSound('mousedown touchstart', '#sound');
+//play button
+const play_btn01 = document.getElementById('play-btn01');
+
+//audio file
+const sound01 = new Audio("sound/sound_01.mp3");
+
+//play event
+play_btn01.addEventListener( 'click' , play );
+
+function play(){
+	sound01.play();
+}
+
+//play button
+const play_btn02 = document.getElementById('play-btn02');
+
+//audio file
+const sound02 = new Audio("sound/sound_02.mp3");
+
+//play event
+play_btn02.addEventListener( 'click' , play );
+
+function play(){
+	sound02.play();
+}
+
+//play button
+const play_btn03 = document.getElementById('play-btn03');
+
+//audio file
+const sound03 = new Audio("sound/sound_03.mp3");
+
+//play event
+play_btn03.addEventListener( 'click' , play );
+
+function play(){
+	sound03.play();
+}
